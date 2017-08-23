08507dba612a255b0485f15577983316a38c2804
mantasBrusokas <mantasbrusokas@gmail.com>
2017-08-23 17:54:27 +0300

    updates

---
+++ js/manto.js
@@ -0,0 +1,16 @@
+function toggleLempute() {
+
+    var skaicius=0;
+    if(skaicius%2==0){
+        skaicus+=1;
+        document.getElementById('lempute').src='../img/pic_bulboff.gif'
+
+    }
+    else {
+        skaicus+=1;
+        document.getElementById('lempute').src='../img/pic_bulbon.gif'
+
+    }
+
+
+}
