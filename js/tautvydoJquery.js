10e7cab2da740aef62147d1034b335ad882ab8a7
Tautvydas <tautvydas@dulskis.eu>
2017-08-24 19:32:00 +0300

    Sukurti tabai. Refactirintas kodas.

--- js/tautvydoJquery.js
+++ js/tautvydoJquery.js
@@ -5,6 +5,14 @@ $(function(){
         $("#togglelemp").click();
     });
 
+    $('.tab').click(function(){
+        var tabId = $(this).attr('href');
 
+        $(tabId).show().siblings().hide();
+
+        $(this).addClass('active').siblings().removeClass('active');
+    });
+
+    $('[data-default]').click();
 
 });
