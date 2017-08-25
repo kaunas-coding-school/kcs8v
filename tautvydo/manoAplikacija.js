c2ae66d042e34ec48767e5f0573ce5a0ceeeef50
Tautvydas <tautvydas@dulskis.eu>
2017-08-25 20:35:00 +0300

    Angular routing

--- tautvydo/manoAplikacija.js
+++ tautvydo/manoAplikacija.js
@@ -1,17 +1,36 @@
-var app = angular.module('manoApplikacija', []);
+var app = angular.module('manoApplikacija', ["ngRoute"]);
 
-app.config(function($routeProvider) {
+app.config(function($routeProvider, $httpProvider, $locationProvider) {
+    $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
     $routeProvider
         .when("/", {
-            templateUrl : "main.htm"
+            templateUrl : "main.html"
         })
-        .when("/red", {
-            templateUrl : "red.htm"
+        .when("/Evaldas", {
+            templateUrl : "Evaldas.html"
         })
-        .when("/green", {
-            templateUrl : "green.htm"
+        .when("/Tautvydas", {
+            templateUrl : "tautvydas.html"
         })
-        .when("/blue", {
-            templateUrl : "blue.htm"
+        .when("/Paulius", {
+            templateUrl : "paulius.html"
+        })
+        .when("/Gytis", {
+            templateUrl : "Gytis.html"
+        })
+        .when("/Vytenis", {
+            templateUrl : "vytenis.html"
+        })
+        .when("/Mantas", {
+            templateUrl : "mantas.html"
+        })
+        .when("/Rytis", {
+            templateUrl : "Rytis.html"
+        })
+        .when("/Andrius", {
+            templateUrl : "andrius.html"
         });
+    $locationProvider.html5Mode(true);
+
 });
--- tautvydo/meniuController.js
+++ tautvydo/meniuController.js
@@ -1,15 +1,13 @@
 app.controller('meniuController', function($scope) {
     console.log('Meniu kontroleris veikia');
     $scope.meniu = [
-        {'pavadinimas' :'Evaldas','adresas':'Evaldas.html'},
-        {'pavadinimas' :'Tautvydas','adresas':'tautvydas.html'},
-        {'pavadinimas' :'Paulius','adresas':'paulius.html'},
-        {'pavadinimas' :'Gytis','adresas':'Gytis.html'},
-        {'pavadinimas' :'Vytenis','adresas':'vytenis.html'},
-        {'pavadinimas' :'Mantas','adresas':'mantas.html'},
-        {'pavadinimas' :'Rytis','adresas':'Rytis.html'},
-        {'pavadinimas' :'Andrius','adresas':'andrius.html'}
+        {'pavadinimas' :'Evaldas','adresas':'/Evaldas'},
+        {'pavadinimas' :'Tautvydas','adresas':'/Tautvydas'},
+        {'pavadinimas' :'Paulius','adresas':'/Paulius'},
+        {'pavadinimas' :'Gytis','adresas':'/Gytis'},
+        {'pavadinimas' :'Vytenis','adresas':'/Vytenis'},
+        {'pavadinimas' :'Mantas','adresas':'/Mantas'},
+        {'pavadinimas' :'Rytis','adresas':'/Rytis'},
+        {'pavadinimas' :'Andrius','adresas':'/Andrius'}
     ];
-    $scope.matomumas=false;
-
 });
